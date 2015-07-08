5e114249f4d6f94f1cedce01200a8d563a27de3a
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2015-07-08 21:45:32 +0200

    7.1/2

--- uebung7/amount.cpp
+++ uebung7/amount.cpp
@@ -1,99 +1,141 @@
 #include <iostream>
+#include <math.h>
+
+enum waehrung {
+	EUR,
+	USD
+};
 
 class Amount {
 		//ToDo 7.2
 		//Implement class Amount
 		double netto, brutto;
-		float mehrwertsteuer;
-		const float from_dollar_to_euro = 0.896205912f;
-		const float from_euro_to_dollar = 1.115815f;
-		enum waehrung {EUR, USD};
+		float mwst;
+		const float USD_TO_EUR = 0.896205912f;
+		const float EUR_TO_USD = 1.115815f;
 		waehrung currency;
 		std::string bezeichnung; 
 	public:
 		Amount();
-		Amount(double startkapital);
-		Amount(double startkapital, float steuersatz);
-		Amount(double startkapital, std::string);
-		Amount(double startkapital, float steuersatz, std::string bezeichnung);
-		bool addBrutto(double add);
-		double getBalance() const;
-		bool convert();
-		double convert(double betrag);
-		bool setSteuer(float tax);
+		Amount(waehrung curr);
+		Amount(double preis);
+		Amount(double preis, waehrung curr);
+		Amount(double preis, float steuersatz);
+		Amount(double preis, float steuersatz, waehrung curr);
+		Amount(double preis, std::string);
+		Amount(double preis, std::string, waehrung curr);
+		Amount(double preis, float steuersatz, std::string bezeichnung);
+		Amount(double preis, float steuersatz, std::string bezeichnung, waehrung curr);
+
+		void changeBrutto(double add);
+		double getNetto() const;
+		double getBrutto() const;
+		void convert();
+		double convert(double value);
 		void print();
 
 };
 
-Amount::Amount() : brutto{0}, mehrwertsteuer{0.19}, bezeichnung{"void"}
+Amount::Amount() : brutto{0}, mwst{0.19}, bezeichnung{""}, currency{EUR}
 {
-	netto = brutto - brutto * mehrwertsteuer;
-	currency = EUR;
-}
-Amount::Amount(double startkapital) : brutto{startkapital}, mehrwertsteuer{0.19}, bezeichnung{""}
-{
-	netto = brutto - brutto * mehrwertsteuer;
-	currency = EUR;
-}
-Amount::Amount(double startkapital, float steuersatz) : brutto{startkapital}, mehrwertsteuer{steuersatz}, bezeichnung{""}
-{
-	netto = brutto - brutto * mehrwertsteuer;
-	currency = EUR;
-}
-Amount::Amount(double startkapital, std::string bezeichnung) : brutto{startkapital}, mehrwertsteuer{0.19}, bezeichnung{bezeichnung}
-{
-	netto = brutto - brutto * mehrwertsteuer;
-	currency = EUR;
-}
-Amount::Amount(double startkapital, float steuersatz, std::string bezeichnung) :
-			   brutto{startkapital}, mehrwertsteuer{steuersatz}, bezeichnung{bezeichnung}
-{
-		netto = brutto - brutto * mehrwertsteuer;
-		currency = EUR;
+	netto = brutto / (1.0 + mwst);
 }
 
-bool Amount::addBrutto(double add)
+Amount::Amount(waehrung curr) : brutto{0}, mwst{0.19}, bezeichnung{""}
 {
-	if(brutto + add >= brutto)
-	{
-		brutto += add;
-		netto = brutto - brutto * mehrwertsteuer;
-		return true;
-	}
-	else return false;
+	netto = brutto / (1.0 + mwst);
+	currency = curr;
 }
 
-double Amount::getBalance() const{
+Amount::Amount(double preis) : netto{preis}, mwst{0.19}, bezeichnung{""}, currency{EUR}
+{
+	brutto = netto * (1.0 + mwst);
+}
+
+Amount::Amount(double preis, waehrung curr) : netto{preis}, mwst{0.19}, bezeichnung{""}
+{
+	brutto = netto * (1.0 + mwst);
+	currency = curr;
+}
+
+Amount::Amount(double preis, float steuersatz) : netto{preis}, mwst{steuersatz}, bezeichnung{""}, currency{EUR}
+{
+	brutto = netto * (1.0 + mwst);
+}
+
+Amount::Amount(double preis, float steuersatz, waehrung curr) : netto{preis}, mwst{steuersatz}, bezeichnung{""}
+{
+	brutto = netto * (1.0 + mwst);
+	currency = curr;
+}
+
+Amount::Amount(double preis, std::string bezeichnung) : netto{preis}, mwst{0.19}, bezeichnung{bezeichnung}, currency{EUR}
+{
+	brutto = netto * (1.0 + mwst);
+}
+
+Amount::Amount(double preis, std::string bezeichnung, waehrung curr) : netto{preis}, mwst{0.19}, bezeichnung{bezeichnung}
+{
+	brutto = netto * (1.0 + mwst);
+	currency = curr;
+}
+
+Amount::Amount(double preis, float steuersatz, std::string bezeichnung) :
+			   netto{preis}, mwst{steuersatz}, bezeichnung{bezeichnung}
+{
+	brutto = netto * (1.0 + mwst);
+	currency = EUR;
+}
+
+Amount::Amount(double preis, float steuersatz, std::string bezeichnung, waehrung curr) :
+			   netto{preis}, mwst{steuersatz}, bezeichnung{bezeichnung}
+{
+	brutto = netto * (1.0 + mwst);
+	currency = curr;
+}
+
+void Amount::changeBrutto(double add)
+{
+	brutto += add;
+	netto = brutto / (1.0 + mwst);
+}
+
+double Amount::getNetto() const{
 	return netto;
 }
 
-bool Amount::convert(){
-	if(currency == EUR)
-	{
-		brutto *= from_euro_to_dollar;
-		netto *= from_euro_to_dollar;
+double Amount::getBrutto() const{
+	return brutto;
+}
+
+void Amount::convert(){
+	double fac = 1.0;
+	if(currency == EUR) {
+		fac = EUR_TO_USD;
 		currency = USD;
-	}
-	else{
-		brutto *= from_dollar_to_euro;
-		netto *= from_dollar_to_euro;
+	} else {
+		fac = USD_TO_EUR;
 		currency = EUR;
 	}
+	brutto *= fac;
+	netto *= fac;	
 }
-double Amount::convert(double betrag)
+
+double Amount::convert(double value)
 {
-	if(currency == EUR)
-	{
-		return betrag *= from_euro_to_dollar;
-	}else 
-		return betrag *= from_dollar_to_euro;
+	if(currency == EUR) {
+		return value *= EUR_TO_USD;
+	} else 
+		return value *= USD_TO_EUR;
 }
 
 
 void Amount::print(){
-	std::cout << "Verfügbares Kapital: " << netto;
-	if(currency == EUR) std::cout << " EUR"; else std::cout << " USD";
-	std::cout <<  std::endl << "Bei einem steuersatz von: " << mehrwertsteuer << "%" << std::endl;
+	std::string cur = currency == EUR ? "EUR" : "USD";
+	if(bezeichnung.size() >= 1)
+		std::cout << bezeichnung << " hat ";
+	std::cout << "Nettopreis: " << round(netto * 100)/100 << " " << cur << ", Bruttopreis: " << round(brutto * 100)/100 << " " << cur << std::endl;
+	std::cout << "Bei einem Mehrwertsteuersatz von: " << 100.0 * mwst << "%" << std::endl;
 }
 
 void test() {
@@ -101,13 +143,45 @@ void test() {
 	//implement tests
 	Amount a1(42);
 	a1.print();
-	a1.addBrutto(50.27);
+	a1.convert();
+	a1.print();
+	a1.changeBrutto(50.27);
 	a1.print();
 	a1.convert();
 	a1.print();
+	
+	std::cout << std::endl;
+	
+	Amount a2(9001, 0.8, USD);
+	a2.print();
+	a2.convert();
+	a2.print();
+	a2.changeBrutto(42);
+	a2.print();
+	a2.convert();
+	a2.print();
 
-	Amount a2(50, "Auto");
+	std::cout << std::endl;
+	
+	Amount a3(1337, "Skills");
+	a3.print();
+	a3.convert();
+	a3.print();
+	a3.changeBrutto(42);
+	a3.print();
+	a3.convert();
+	a3.print();
 
+	std::cout << std::endl;
+	
+	Amount a4(911, 0.10, "JETFUELCANTMELTSTEELBEAMS", USD);
+	a4.print();
+	a4.convert();
+	a4.print();
+	a4.changeBrutto(1337);
+	a4.print();
+	a4.convert();
+	a4.print();
 }
 
 int main() {
